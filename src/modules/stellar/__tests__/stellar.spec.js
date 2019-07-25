9371caa4638248cde8fc34985f04d23c988055c4
Tim <tim@Tims-MacBook-Pro.local>
2019-07-25 16:07:52 +0700

    test: refactor test cases format based on rspec

--- src/modules/stellar/__tests__/stellar.spec.js
+++ src/modules/stellar/__tests__/stellar.spec.js
@@ -44,92 +44,105 @@ describe('Stellar', () => {
     mockServer.close(true);
   });
 
-  it('should get a stellar sequenceNumber correctly', async () => {
-    let seq = await client.getSequenceNumber(senderpk);
-    expect(seq.sequenceNumber).toBe(currentSeq);
-  });
+  describe('When get a stellar sequence number', () => {
+    describe('With invalid input', () => {
+        it('should fail to get a stellar sequenceNumber', async () => {
+            await expect(client.getSequenceNumber('Bad')).rejects.toThrow(StellarException);
+          });
+    })
+    describe('With valid input', () => {
+        it('should get a stellar sequenceNumber correctly', async () => {
+            let seq = await client.getSequenceNumber(senderpk);
+            expect(seq.sequenceNumber).toBe(currentSeq);
+        });
+    })
+  })
 
-  it('should fail to get a stellar sequenceNumber, invalid input', async () => {
-    await expect(client.getSequenceNumber('Bad')).rejects.toThrow(StellarException);
-  });
+  describe('When deposit transaction', () => {
+      describe('With valid input', () => {
+        it('should create a correct deposit payment transaction', async () => {
+            let amount = '100.0000000';
+            let res = await client.getSequenceNumber(senderpk);
+            const xlm = getLumensAsset()
+            let txeB64 = await client.createDepositTx({
+                src: sender, 
+                seq: res.sequenceNumber, 
+                amount, 
+                asset: xlm,
+            });
+            let tx = new StellarSDK.Transaction(txeB64);
+        
+            // validate sender pk
+            expect(tx.source).toBe(senderpk);
+        
+            // validate sender seq
+            expect(tx.sequence).toBe(nextSeq);
+        
+            // validate timeBounds
+            expect(tx.timeBounds).toBeUndefined();
+        
+            // validate payment operation
+            expect(tx.operations.length).toBe(1);
+        
+            let paymentOp = tx.operations[0];
+            expect(paymentOp.type).toBe('payment');
+            expect(paymentOp.source).toBe(senderpk);
+            expect(paymentOp.destination).toBe('GAAQ4EOKRV3O5MC42JPREIUYRCTXUE6JLXWHMETM24AFACXWE54FQATQ');
+            expect(paymentOp.asset.code).toBe(xlm.asset.code);
+            expect(paymentOp.asset.issuer).toBe(xlm.asset.issuer);
+            expect(paymentOp.amount).toBe(amount);
+        
+            // validate signature
+            const sigsObj = tx.signatures;
+            let signatures = sigsObj.map((s) => s);
+            expect(signatures.length).toBe(1);
+            let kp = StellarSDK.Keypair.fromPublicKey(senderpk);
+            expect(kp.verify(tx.hash(), signatures[0].signature())).toBeTruthy();
+          });        
+      })
+  })
 
-  it('should create a correct deposit payment transaction', async () => {
-    let amount = '100.0000000';
-    let res = await client.getSequenceNumber(senderpk);
-    const xlm = getLumensAsset()
-    let txeB64 = await client.createDepositTx({
-        src: sender, 
-        seq: res.sequenceNumber, 
-        amount, 
-        asset: xlm,
-    });
-    let tx = new StellarSDK.Transaction(txeB64);
-
-    // validate sender pk
-    expect(tx.source).toBe(senderpk);
-
-    // validate sender seq
-    expect(tx.sequence).toBe(nextSeq);
-
-    // validate timeBounds
-    expect(tx.timeBounds).toBeUndefined();
-
-    // validate payment operation
-    expect(tx.operations.length).toBe(1);
-
-    let paymentOp = tx.operations[0];
-    expect(paymentOp.type).toBe('payment');
-    expect(paymentOp.source).toBe(senderpk);
-    expect(paymentOp.destination).toBe('GAAQ4EOKRV3O5MC42JPREIUYRCTXUE6JLXWHMETM24AFACXWE54FQATQ');
-    expect(paymentOp.asset.code).toBe(xlm.asset.code);
-    expect(paymentOp.asset.issuer).toBe(xlm.asset.issuer);
-    expect(paymentOp.amount).toBe(amount);
-
-    // validate signature
-    const sigsObj = tx.signatures;
-    let signatures = sigsObj.map((s) => s);
-    expect(signatures.length).toBe(1);
-    let kp = StellarSDK.Keypair.fromPublicKey(senderpk);
-    expect(kp.verify(tx.hash(), signatures[0].signature())).toBeTruthy();
-  });
-
-  it('should create a correct withdraw payment transaction', async () => {
-    let amount = '100.0000000';
-    let res = await client.getSequenceNumber(senderpk);
-    const xlm = getLumensAsset()
-    let txeB64 = await client.createWithdrawTx({
-        src: sender, 
-        seq: res.sequenceNumber, 
-        amount, 
-        asset: xlm
-    });
-    let tx = new StellarSDK.Transaction(txeB64);
-
-    // validate sender pk
-    expect(tx.source).toBe(senderpk);
-
-    // validate sender seq
-    expect(tx.sequence).toBe(nextSeq);
-
-    // validate timeBounds
-    expect(tx.timeBounds).toBeUndefined();
-
-    // validate payment operation
-    expect(tx.operations.length).toBe(1);
-
-    let paymentOp = tx.operations[0];
-    expect(paymentOp.type).toBe('payment');
-    expect(paymentOp.source).toBe('GAAQ4EOKRV3O5MC42JPREIUYRCTXUE6JLXWHMETM24AFACXWE54FQATQ');
-    expect(paymentOp.destination).toBe(senderpk);
-    expect(paymentOp.asset.code).toBe(xlm.asset.code);
-    expect(paymentOp.asset.issuer).toBe(xlm.asset.issuer);
-    expect(paymentOp.amount).toBe(amount);
-
-    // validate signature
-    const sigsObj = tx.signatures;
-    let signatures = sigsObj.map((s) => s);
-    expect(signatures.length).toBe(1);
-    let kp = StellarSDK.Keypair.fromPublicKey(senderpk);
-    expect(kp.verify(tx.hash(), signatures[0].signature())).toBeTruthy();
-  });
+  describe('When withdraw transaction', () => {
+      describe('With valid input', () => {
+        it('should create a correct withdraw payment transaction', async () => {
+            let amount = '100.0000000';
+            let res = await client.getSequenceNumber(senderpk);
+            const xlm = getLumensAsset()
+            let txeB64 = await client.createWithdrawTx({
+                src: sender, 
+                seq: res.sequenceNumber, 
+                amount, 
+                asset: xlm
+            });
+            let tx = new StellarSDK.Transaction(txeB64);
+        
+            // validate sender pk
+            expect(tx.source).toBe(senderpk);
+        
+            // validate sender seq
+            expect(tx.sequence).toBe(nextSeq);
+        
+            // validate timeBounds
+            expect(tx.timeBounds).toBeUndefined();
+        
+            // validate payment operation
+            expect(tx.operations.length).toBe(1);
+        
+            let paymentOp = tx.operations[0];
+            expect(paymentOp.type).toBe('payment');
+            expect(paymentOp.source).toBe('GAAQ4EOKRV3O5MC42JPREIUYRCTXUE6JLXWHMETM24AFACXWE54FQATQ');
+            expect(paymentOp.destination).toBe(senderpk);
+            expect(paymentOp.asset.code).toBe(xlm.asset.code);
+            expect(paymentOp.asset.issuer).toBe(xlm.asset.issuer);
+            expect(paymentOp.amount).toBe(amount);
+        
+            // validate signature
+            const sigsObj = tx.signatures;
+            let signatures = sigsObj.map((s) => s);
+            expect(signatures.length).toBe(1);
+            let kp = StellarSDK.Keypair.fromPublicKey(senderpk);
+            expect(kp.verify(tx.hash(), signatures[0].signature())).toBeTruthy();
+          });
+      })
+  })
 });
